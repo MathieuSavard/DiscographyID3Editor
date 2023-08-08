99fe7cfb2fd5174129c976b37648ba7180feb78e
Mathieu Savard <mathieusavard@hotmail.com>
2023-08-08 13:24:19 -0400

    Work in Progress

--- Project_POC/main.cpp
+++ Project_POC/main.cpp
@@ -1,6 +1,6 @@
-#include "application.hpp"
+#include "app.hpp"
 //#include "file_system/boost/boost_file_system.hpp"
-#include "id3_editor/taglib/taglib_id3_editor.hpp"
+#include "taglib_id3_editor.hpp"
 
 int main(int argc, char* argv[]) {
   Application application(NULL, NULL);
