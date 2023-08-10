58a03e6591190ae89ba5c12c1d7b06ff28ff65d8
Mathieu Savard <mathieusavard@hotmail.com>
2023-08-09 20:05:03 -0400

    x

---
+++ Project/script/TagLib_Setup.py
@@ -0,0 +1,24 @@
+import os
+import shutil
+import subprocess
+
+
+BuildFolderName = "build"
+RootPath = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TagLibPath = os.path.join(RootPath, "/")
+BuildPath = os.path.join(TagLibPath, BuildFolderName)
+
+if os.path.exists(BuildPath):
+        shutil.rmtree(BuildPath)
+os.mkdir(BuildPath)
+
+
+if os.name == 'nt':
+    cmake_command = 'cmake -G "MinGW Makefiles" -B "{}" -S "{}"'.format(BuildPath, RootPath)
+    make_command = 'mingw32-make.exe'
+else:
+    cmake_command = 'cmake -B "{}" -S "{}"'.format(BuildPath, RootPath)
+    make_command = 'make'
+
+subprocess.run(cmake_command, shell=True, cwd=RootPath)
+subprocess.run(make_command, shell=True, cwd=BuildPath)
