8f42367fc51a65385117a985c72058e432116149
Mathieu Savard <mathieusavard@hotmail.com>
2024-02-27 20:12:14 -0500

    Add Build and Compile project scripts

--- Project/script/Compile_Project.py
+++ Project/script/Compile_Project.py
@@ -4,11 +4,6 @@ import subprocess
 import platform
 import sys
 
-def create_build_directory(path):
-    if os.path.exists(path):
-        shutil.rmtree(path)
-    os.mkdir(path)
-
 def run_command(command, cwd=None):
     subprocess.run(command, shell=True, cwd=cwd)
 
@@ -18,15 +13,13 @@ def main():
     BuildPath = os.path.join(RootPath, BuildFolderName)
 
     if platform.system() == "Windows":
-        cmake_command = 'cmake -G "MinGW Makefiles" -B "{}" -S "{}"'.format(BuildPath, RootPath)
+        make_command = 'mingw32-make.exe'
     elif platform.system() == "Linux":
-        cmake_command = 'cmake -B "{}" -S "{}"'.format(BuildPath, RootPath)
+        make_command = 'make'
     else:
         sys.exit()
-
-    create_build_directory(BuildPath)
     
-    run_command(cmake_command, cwd=RootPath)
+    run_command(make_command, cwd=BuildPath)
 
 if __name__ == "__main__":
     main()
